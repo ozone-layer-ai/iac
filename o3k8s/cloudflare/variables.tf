59892ce7e2e59539cbc4ea5d992664fb4eb0bb03
Stefan Todorov <stf.todoroff@gmail.com>
2026-02-23 15:16:37 +0200

    Add monitoring subdomains to tenant clusters

--- o3k8s/cloudflare/variables.tf
+++ o3k8s/cloudflare/variables.tf
@@ -1,3 +1,17 @@
 variable "domain" {}
 
 variable "cluster" {}
+
+variable "orgid" {}
+
+variable "metrics_subdomain" {
+  default = "metrics"
+}
+
+variable "logs_subdomain" {
+  default = "logs"
+}
+
+variable "observe_subdomain" {
+  default = "observe"
+}

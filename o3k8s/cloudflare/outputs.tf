db549ee71d1d51885098bcdbc77af4797c2d9178
Stefan Todorov <stf.todoroff@gmail.com>
2026-02-25 10:48:56 +0200

    Add nat_gw_ips output

--- o3k8s/cloudflare/outputs.tf
+++ o3k8s/cloudflare/outputs.tf
@@ -2,6 +2,10 @@ output "tunnel_token" {
   value = data.cloudflare_zero_trust_tunnel_cloudflared_token.cf_tunnel_token.token
 }
 
+locals {
+  cluster_access_url_apex=replace(cloudflare_dns_record.this.name, data.cloudflare_zone.this.name, "")
+}
+
 output "cluster_access_url" {
-  value = "${cloudflare_dns_record.this.name}.${data.cloudflare_zone.this.name}"
+  value = "${local.cluster_access_url_apex}${data.cloudflare_zone.this.name}"
 }
